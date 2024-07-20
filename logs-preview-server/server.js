fd10189c73d6b6700b971677208a697dcdd79444
Mayowa Sodipo <sodipomayowa7@gmail.com>
2024-07-20 20:34:31 +0100

    fix:deployment

--- logs-preview-server/server.js
+++ logs-preview-server/server.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const readline = require('readline');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const readline = require("readline");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 const PORT = 7000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', async (req, res) => {
-    const { url } = req.query;
-    // read from a file line by line
-    const filePath = '/home/dominic-source/Projects/interns-bot-4-docker-deployment/my-first-app/deployment.log'
-    const logs = [];
+app.get("/", async (req, res) => {
+  const { url } = req.query;
+  // read from a file line by line
+  const filePath = process.env.FILE_PATH_LOG + "/deployment.log";
+  const logs = [];
 
-    const readInterface = readline.createInterface({
-        input: fs.createReadStream(filePath)
-    });
-    for await (const line of readInterface) {
-        logs.push(line);
-    }
+  const readInterface = readline.createInterface({
+    input: fs.createReadStream(filePath),
+  });
+  for await (const line of readInterface) {
+    logs.push(line);
+  }
 
-    res.render('index', { logs, url });
+  res.render("index", { logs, url });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
--- my-first-app/scripts/deploy.sh
+++ my-first-app/scripts/deploy.sh
@@ -2,35 +2,6 @@
 
 # set -e
 
-# BRANCH_NAME=$1 
-# PR_NUMBER=$2  
-# REPO_NAME=$3
-# REPO_URL=$4
-# APP_DIR="${HOME}/my-first-app-${PR_NUMBER}"
-# DOCKER_COMPOSE_PROJECT_NAME="${REPO_NAME}-${PR_NUMBER}"
-
-# #ssh to be stored here
-# # Navigate to the application directory
-
-# mkdir -p $APP_DIR
-# rm -rf $APP_DIR/*
-# git clone $REPO_URL $APP_DIR
-# cd $APP_DIR
-# # Pull the latest code from the specified branch
-# echo "Pulling latest code from branch ${BRANCH_NAME}..."
-# git fetch origin
-# git checkout $BRANCH_NAME
-# git pull origin $BRANCH_NAME
-
-# # run the Docker Compose
-# echo "Building and running Docker Compose services..."
-# docker-compose -p $DOCKER_COMPOSE_PROJECT_NAME up -d --build
-
-# echo "Deployment for PR #${PR_NUMBER} completed successfully."
-
-
-# #!/bin/bash
-
 
 # Variables
  REPO_URL=$1
